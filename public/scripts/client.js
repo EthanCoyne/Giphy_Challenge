04817e7e36f038346d409712acfc00b5cf1573a7
Ethan Coyne <ethancoyne@Ethans-MacBook-Pro.local>
2017-02-02 14:19:15 -0600

    updated app to use services

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,30 +1,27 @@
 var app = angular.module('giphyApp', []);
 
-app.controller('giphyController', function($http){
+app.controller('giphyController', function(GiphyService){
   console.log("giphyController Loaded");
 
   var ctrl = this;
-  var API = "https://api.giphy.com/v1/";
-  var params = {params: {api_key: 'dc6zaTOxFJmzC'}};
-  ctrl.searchTerm = '';
+  // ctrl.searchTerm = '';
+
+
 
   //API KEYs , { params: { api_key: 'key goes here'}}
-  ctrl.randomGIF = function(){
-    $http.get(API + "gifs/random", params).then(function(response){
-      console.log(response);
-      ctrl.imageName = response.data.data.id;
-      ctrl.imageURL = response.data.data.image_url;
-      console.log('img URL: ', ctrl.imageURL);
-    });
+  ctrl.randomGIF = function () {
+    GiphyService.randomGIF().then(function(gif) {
+        ctrl.imageName = gif.data.id;
+        ctrl.imageURL = gif.data.image_url;
+        console.log('img URL: ', ctrl.imageURL);
+      });
   };
 
-  ctrl.searchGIF = function(){
-    params.params.q = ctrl.searchTerm.split(' ').join('+');
-    console.log(params.params.q);
-    $http.get(API + "gifs/search", params).then(function(response){
-      console.log(response);
-      ctrl.imageName = response.data.data[0].id;
-      ctrl.imageURL = response.data.data[0].images.original.url;
+
+  ctrl.searchGIF = function(searchTerm){
+    GiphyService.searchGIF(searchTerm).then(function(gif) {
+      ctrl.imageName = gif.data[0].id;
+      ctrl.imageURL = gif.data[0].images.original.url;
       console.log('img URL: ', ctrl.imageURL);
     });
   }
---
+++ public/scripts/giphy-service.js
@@ -0,0 +1,34 @@
+app.service('GiphyService', function($http) {
+  var API = "https://api.giphy.com/v1/";
+  var params = {params: {api_key: 'dc6zaTOxFJmzC'}};
+
+
+  this.randomGIF = function() {
+    return $http.get(API + "gifs/random", params).then(function(response) {
+      console.log('gif received: ', response);
+      return response.data;
+  });
+}
+
+this.searchGIF = function(searchTerm) {
+  params.params.q = searchTerm.split(' ').join('+');
+
+  return $http.get(API + "gifs/search", params).then(function(response) {
+    return response.data;
+  });
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});//end app.service
